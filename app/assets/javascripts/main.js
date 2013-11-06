7362b74a14f56a1342f0542d8db60eb0b8208618
Matt Lucas <mattlucas@gmail.com>
2013-11-06 11:18:32 -0500

    amy added a bit of jMagiq

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -5,7 +5,7 @@ function sendMessage() {
   var form = new FormData();
   form.title = this.elements['title'].value;
   form.body = this.elements['body'].value;
-  form.isread = this.elements['is_read'].value;
+  form.is_read = this.elements['is_read'].value;
   var xhr = new XMLHttpRequest();
   xhr.open("post", "/messages", false);
   xhr.send(form);
