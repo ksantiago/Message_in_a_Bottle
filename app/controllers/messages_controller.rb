0d692a7698b6d213a95c987f1bcae069b79e07be
Matt Lucas <mattlucas@gmail.com>
2013-11-06 11:05:00 -0500

    routing for the form

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,11 +2,22 @@ class MessagesController < ApplicationController
   respond_to :json
 
   def index
-
+    @messages = Message.all
+    respond_with @messages
   end
 
   def create
+    @message = Message.create(params[:message])
+  end
 
+  def show
+    @message = Message.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
   end
 
 end
