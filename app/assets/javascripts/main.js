e15d7c15b8a61b1bcc75b20a65149e2fc2037ad5
Amy Ruan <nyc.amy@gmail.com>
2013-11-06 11:15:38 -0500

    more js

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,15 +2,13 @@
 
 
 function sendMessage() {
-  var form = new FormData();
-  form.title = this.elements['title'].value;
-  form.body = this.elements['body'].value;
-  form.isread = this.elements['isread'].value;
+  var form = new FormData;
+  form.append("message[title]", this.elements['title'].value);
+  form.append("message[body]", this.elements['body'].value);
+  form.append("message[is_read]", this.elements['is_read'].value);
   var xhr = new XMLHttpRequest();
-  xhr.open("post","/crew",false);
+  xhr.open("post", "/messages", false);
   xhr.send(form);
 }
 
-
-
-$('button').on("click",sendMessage)
+$('button').on("click", sendMessage)
