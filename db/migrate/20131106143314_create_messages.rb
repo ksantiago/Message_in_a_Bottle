9a634d8e96cf88c4bf7319ab2782e4e4e607d1c0
Matt Lucas <mattlucas@gmail.com>
2013-11-06 10:02:35 -0500

    updated the migration table and added kat's routes

--- db/migrate/20131106143314_create_messages.rb
+++ db/migrate/20131106143314_create_messages.rb
@@ -1,7 +1,11 @@
 class CreateMessages < ActiveRecord::Migration
-  def up
-  end
-
-  def down
+  def change
+    create_table :messages do |t|
+        t.integer :id
+        t.string :body
+        t.string :message
+        t.is_read :boolean
+        t.timestamps
+    end
   end
 end
