f0d6ad70d4b79ad99c56357253b88d1104d6ad1b
Katherine Santiago <santiago.kat@gmail.com>
2013-11-06 10:24:32 -0500

    added index for messages views

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20131106143314) do
+
+  create_table "messages", :force => true do |t|
+    t.string   "body"
+    t.string   "message"
+    t.boolean  "is_read"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
